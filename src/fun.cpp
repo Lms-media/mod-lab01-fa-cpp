354e21fee29964662bec6fd542adb810d358c4c3
Lms-media <maksimap2013@gmail.com>
2025-03-14 20:02:56 +0300

    faStr2 implementation

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,6 +2,8 @@
 #include "fun.h"
 
 bool isNum(char);
+bool isLetter(char);
+bool isCapital(char);
 
 unsigned int faStr1(const char *str) {
   bool inWord = false;
@@ -25,8 +27,35 @@ unsigned int faStr1(const char *str) {
   return count;
 }
 
-bool isNum(char c) { return c >= '0' && c <= '9'; }
-
-unsigned int faStr2(const char *str) { return 0; }
+unsigned int faStr2(const char *str) {
+  bool inWord = false;
+  bool correct = false;
+  unsigned int count = 0;
+  int i = 0;
+  while (str[i] != '\0') {
+    if (inWord && !isLetter(str[i]) && correct) {
+      count--;
+      correct = false;
+    }
+    if (!inWord && str[i] != ' ') {
+      inWord = true;
+      if (isCapital(str[i])) {
+        count++;
+        correct = true;
+      }
+    } else if (inWord && str[i] == ' ') {
+      inWord = false;
+      correct = false;
+    }
+    i++;
+  }
+  return count;
+}
 
 unsigned int faStr3(const char *str) { return 0; }
+
+bool isNum(char c) { return c >= '0' && c <= '9'; }
+
+bool isLetter(char c) { return (c >= 'a' && c <= 'z') || c == ' '; }
+
+bool isCapital(char c) { return c >= 'A' && c <= 'Z'; }
