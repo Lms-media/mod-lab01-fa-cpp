3a9bb46261e422cb2e0b16da2f362f99e79e98e4
Lms-media <maksimap2013@gmail.com>
2025-03-14 09:01:48 +0300

    faStr1 implementation

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,32 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
 
+bool isNum(char);
+
 unsigned int faStr1(const char *str) {
-    return 0;
+  bool inWord = false;
+  bool num = false;
+  unsigned int count = 0;
+  int i = 0;
+  while (str[i] != '\0') {
+    if (!inWord && str[i] != ' ') {
+      inWord = true;
+      count++;
+    } else if (inWord && str[i] == ' ') {
+      inWord = false;
+      num = false;
+    }
+    if (inWord && isNum(str[i]) && !num) {
+      num = true;
+      count--;
+    }
+    i++;
+  }
+  return count;
 }
 
-unsigned int faStr2(const char *str) {
-    return 0;
-}
+bool isNum(char c) { return c >= '0' && c <= '9'; }
 
-unsigned int faStr3(const char *str) {
-    return 0;
-}
+unsigned int faStr2(const char *str) { return 0; }
+
+unsigned int faStr3(const char *str) { return 0; }
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,6 @@
 // Copyright 2022 UNN-IASR
-#include <iostream>
 #include "fun.h"
 
 int main() {
-    int f = faStr1("gg");
-    std::cout<<f<<std::endl;
-    return 0;
+  return 0;
 }
